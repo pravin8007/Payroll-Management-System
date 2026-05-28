2ebae288ab52b827bfeb0033a020a388e0961989
pravin patil <pravinshivajipatil37@gmail.com>
2026-05-28 21:47:06 +0530

    Maked Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,45 @@
-import { Routes, Route, NavLink } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
-import Employees from './pages/Employees';
-import Attendance from './pages/Attendance';
-import Payroll from './pages/Payroll';
+import { Routes, Route, NavLink } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Employees from "./pages/Employees";
+import Attendance from "./pages/Attendance";
+import Payroll from "./pages/Payroll";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <nav className="w-full bg-white shadow-md p-4 flex items-center justify-between">
+    <div className="min-h-screen bg-gray-100 overflow-x-hidden">
+      <nav className="w-full bg-white shadow-md fixed top-0 z-50 ">
+        <div className="max-w-full mx-10 py-2.5 flex flex-col md:flex-row sm:items-center justify-between">
+          <h1 className="text-2xl font-bold text-gray-700 text-center sm:text-left">
+            Payroll Management System
+          </h1>
 
-        <h1 className="font-semibold text-gray-600">Payroll Management System</h1>
-
-        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '2.5rem' }}>
-          {[
-            { to: '/', label: 'Dashboard' },
-            { to: '/employees', label: 'Employees' },
-            { to: '/attendance', label: 'Attendance' },
-            { to: '/payroll', label: 'Payroll' },
-          ].map(({ to, label }) => (
-            <NavLink
-              key={to}
-              to={to}
-              end={to === '/'}
-              style={{ textDecoration: 'none' }}
-              className={({ isActive }) =>
-                `text-sm font-medium transition-colors ${isActive ? 'text-blue-600' : 'text-gray-500 hover:text-gray-900'
-                }`
-              }
-            >
-              {label}
-            </NavLink>
-          ))}
+          <div className="flex items-center justify-center sm:justify-end mt-3 md:mt-0 gap-2 sm:gap-4">
+            {[
+              { to: "/", label: "Dashboard" },
+              { to: "/employees", label: "Employees" },
+              { to: "/attendance", label: "Attendance" },
+              { to: "/payroll", label: "Payroll" },
+            ].map(({ to, label }) => (
+              <NavLink
+                key={to}
+                to={to}
+                end={to === "/"}
+                className={({ isActive }) =>
+                  `px-3 py-2 text-sm sm:text-base font-medium transition-all duration-200
+                    ${isActive
+                    ? "text-blue-600"
+                    : "text-gray-500 hover:text-gray-900"
+                  }`
+                }
+              >
+                {label}
+              </NavLink>
+            ))}
+          </div>
         </div>
-
       </nav>
 
-      <main className="p-6">
+     <main className="pt-28 md:pt-20 p-6">
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/employees" element={<Employees />} />
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from 'react-redux';
-import {Users,CalendarCheck,IndianRupee,FileText,} from 'lucide-react';
+import { Users, CalendarCheck, IndianRupee, FileText, } from 'lucide-react';
 
 function Dashboard() {
   const employees = useSelector(
@@ -24,156 +24,161 @@ function Dashboard() {
   );
 
   return (
-    <div className="space-y-8">
+    <div className="space-y-6 sm:space-y-8 p-3 sm:p-5">
 
-      <div>
-        <h1 className="text-3xl font-bold text-gray-800">
+      <div className="space-y-1 sm:flex sm:items-center sm:justify-between sm:space-y-0">
+        <h1 className="text-2xl sm:text-3xl font-bold text-gray-800">
           Payroll Dashboard
         </h1>
 
-        <p className="text-gray-500 mt-2">
-          Manage employees, attendance, payroll,
-          and payslips.
+        <p className="text-sm sm:text-base text-gray-500 mt-2">
+          Manage employees, attendance, payroll and payslips.
         </p>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-        <div className="bg-white rounded-xl shadow p-6 border">
+      <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4 sm:gap-6">
+
+        <div className="bg-white rounded-xl shadow p-4 sm:p-6 border">
           <div className="flex items-center justify-between">
             <div>
-              <p className="text-gray-500">
+              <p className="text-sm sm:text-base text-gray-500">
                 Total Employees
               </p>
 
-              <h2 className="text-3xl font-bold mt-2">
+              <h2 className="text-2xl sm:text-3xl font-bold mt-2">
                 {totalEmployees}
               </h2>
             </div>
 
-            <div className="bg-blue-100 p-3 rounded-full">
-              <Users className="text-blue-600" />
+            <div className="bg-blue-100 p-2 sm:p-3 rounded-full">
+              <Users className="text-blue-600 w-5 h-5 sm:w-6 sm:h-6" />
             </div>
           </div>
         </div>
 
-        <div className="bg-white rounded-xl shadow p-6 border">
+        <div className="bg-white rounded-xl shadow p-4 sm:p-6 border">
           <div className="flex items-center justify-between">
             <div>
-              <p className="text-gray-500">
+              <p className="text-sm sm:text-base text-gray-500">
                 Attendance Records
               </p>
 
-              <h2 className="text-3xl font-bold mt-2">
+              <h2 className="text-2xl sm:text-3xl font-bold mt-2">
                 {totalAttendance}
               </h2>
             </div>
 
-            <div className="bg-green-100 p-3 rounded-full">
-              <CalendarCheck className="text-green-600" />
+            <div className="bg-green-100 p-2 sm:p-3 rounded-full">
+              <CalendarCheck className="text-green-600 w-5 h-5 sm:w-6 sm:h-6" />
             </div>
           </div>
         </div>
 
 
-        <div className="bg-white rounded-xl shadow p-6 border">
+        <div className="bg-white rounded-xl shadow p-4 sm:p-6 border">
           <div className="flex items-center justify-between">
             <div>
-              <p className="text-gray-500">
+              <p className="text-sm sm:text-base text-gray-500">
                 Monthly Payroll
               </p>
 
-              <h2 className="text-3xl font-bold mt-2">
+              <h2 className="text-xl sm:text-3xl font-bold mt-2 wrap-break-word">
                 ₹ {totalPayroll.toLocaleString()}
               </h2>
             </div>
 
-            <div className="bg-yellow-100 p-3 rounded-full">
-              <IndianRupee className="text-yellow-600" />
+            <div className="bg-yellow-100 p-2 sm:p-3 rounded-full">
+              <IndianRupee className="text-yellow-600 w-5 h-5 sm:w-6 sm:h-6" />
             </div>
           </div>
         </div>
 
-
-        <div className="bg-white rounded-xl shadow p-6 border">
+        <div className="bg-white rounded-xl shadow p-4 sm:p-6 border">
           <div className="flex items-center justify-between">
             <div>
-              <p className="text-gray-500">
+              <p className="text-sm sm:text-base text-gray-500">
                 Generated Payslips
               </p>
 
-              <h2 className="text-3xl font-bold mt-2">
+              <h2 className="text-2xl sm:text-3xl font-bold mt-2">
                 {payrolls.length}
               </h2>
             </div>
 
-            <div className="bg-purple-100 p-3 rounded-full">
-              <FileText className="text-purple-600" />
+            <div className="bg-purple-100 p-2 sm:p-3 rounded-full">
+              <FileText className="text-purple-600 w-5 h-5 sm:w-6 sm:h-6" />
             </div>
           </div>
         </div>
       </div>
 
 
-      <div className="bg-white rounded-xl shadow border p-6">
-        <h2 className="text-2xl font-bold mb-4">
+      <div className="bg-white rounded-xl shadow border p-4 sm:p-6">
+        <h2 className="text-xl sm:text-2xl font-bold mb-4">
           Employee Overview
         </h2>
 
         {employees.length === 0 ? (
-          <p className="text-gray-500">
+          <p className="text-gray-500 text-sm sm:text-base">
             No employees added yet.
           </p>
         ) : (
           <div className="overflow-x-auto">
-            <table className="w-full border-collapse">
+            <table className="min-w-175 w-full border-collapse">
               <thead>
                 <tr className="bg-gray-100">
-                  <th className="text-left p-3 border">
+                  <th className="text-left p-3 border text-sm sm:text-base">
                     ID
                   </th>
 
-                  <th className="text-left p-3 border">
+                  <th className="text-left p-3 border text-sm sm:text-base">
                     Name
                   </th>
 
-                  <th className="text-left p-3 border">
+                  <th className="text-left p-3 border text-sm sm:text-base">
                     Department
                   </th>
 
-                  <th className="text-left p-3 border">
+                  <th className="text-left p-3 border text-sm sm:text-base">
                     Salary
                   </th>
                 </tr>
               </thead>
+
               <tbody>
                 {employees.map(emp => (
-                  <tr key={emp.id}>
+                  <tr
+                    key={emp.id}
+                    className="hover:bg-gray-50 transition"
+                  >
                     <td className="p-3 border">
-                      <div className="flex items-center gap-3">
-                        <span>{emp.id}</span>
+                      <div className="flex flex-col sm:flex-row sm:items-center gap-2">
+                        <span className="text-sm">
+                          {emp.id}
+                        </span>
+
                         <button
                           onClick={() =>
                             navigator.clipboard.writeText(
                               emp.id
                             )
                           }
-                          className="bg-blue-100 hover:bg-blue-200 text-blue-600 px-2 py-1 rounded text-sm"
+                          className="bg-blue-100 hover:bg-blue-200 text-blue-600 px-2 py-1 rounded text-xs sm:text-sm w-fit"
                         >
                           Copy
                         </button>
                       </div>
                     </td>
 
-                    <td className="p-3 border">
+                    <td className="p-3 border text-sm sm:text-base">
                       {emp.name}
                     </td>
 
-                    
-                    <td className="p-3 border">
+                    <td className="p-3 border text-sm sm:text-base">
                       {emp.department}
                     </td>
 
-                    <td className="p-3 border">
+                    <td className="p-3 border text-sm sm:text-base whitespace-nowrap">
                       ₹{' '}
                       {Number(emp.salary).toLocaleString()}
                     </td>
